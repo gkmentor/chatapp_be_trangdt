f2b20f2ca466d5a0d767b30df9e7a139aa6ea3b9
giakinh0823 <giakinh2000@gmail.com>
2024-12-15 22:51:51 +0700

    fix

--- src/main/java/springboot/chatapp/controller/PingController.java
+++ src/main/java/springboot/chatapp/controller/PingController.java
@@ -1,15 +1,19 @@
 package springboot.chatapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("api")
 public class PingController {
 
-    @GetMapping("ping")
-    public String ping() {
-        return "pong";
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        log.info("ping");
+        return ResponseEntity.ok("pong");
     }
 }
